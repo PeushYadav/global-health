8c78a5bd6a39cb878fcf3f0192fde4e5771ba9db
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-09-20 15:27:18 +0530

    hero-section responsive

--- src/components/herosection.tsx
+++ src/components/herosection.tsx
@@ -3,23 +3,23 @@ import Tag from "./ui/tag";
 
 const HeroSection = () => {
   return (
-    <div className="relative overflow-hidden px-20 py-0 my-5 h-[600px] sm:h-auto grid grid-cols-[2fr_1fr] gap-8 text-black font-[aspekta] ">
+    <div className="max-w-7xl mx-auto 2xl:max-w-full overflow-hidden px-2 md:px-20 py-0 my-5 h-auto sm:h-auto grid grid-cols-1 2xl:grid-cols-[2fr_1fr] gap-8 text-black font-[aspekta] ">
       {/* First column (image + text) */}
-      <div className="relative col-start-1 h-[700px] flex items-center">
+      <div className="relative 2xl:col-start-1 h-[450px] lg:h-[700px] flex items-center">
         {/* Background image with fixed size */}
         <img
           src="/girl-phone-right.svg"
           alt="girl with phone"
-          className="absolute left-50 top-0 w-[900px] h-auto z-10"
+          className="absolute hidden md:block right-0 top-0 w-[600px] lg:w-[900px] h-auto z-10"
         />
 
         {/* Foreground content */}
-        <div className="relative z-10 flex flex-col justify-center items-start">
+        <div className="relative z-10 flex flex-col justify-center items-start mt-0">
           <div className="flex justify-start items-center mb-4">
             <Tag />
           </div>
-          <p className=" text-8xl text-black"><span className='font-bold'>Med</span>Track</p>
-          <p className="mt-4 text-lg text-gray-700 w-[300px] max-w-md">
+          <p className=" text-4xl lg:text-8xl text-black"><span className='font-bold'>Med</span>Track</p>
+          <p className="mt-4 text-sm md:text-lg text-gray-700 w-[300px] max-w-md">
             Revolutionize chronic disease management with AI-powered health
             tracking.
           </p>
@@ -36,9 +36,26 @@ const HeroSection = () => {
       </div>
 
       {/* Second column (Reminders + Meet Ayu) */}
-      <div className="col-start-2 flex flex-col gap-6 overflow-x-hidden">
+      <div className="2xl:col-start-2 2xl:flex flex flex-col-reverse md:grid md:grid-cols-[1fr_1fr] lg:grid-cols-[2fr_1fr] 2xl:flex-col-reverse gap-6 overflow-x-hidden">
+       {/* Meet Ayu card */}
+        <div className="bg-gray-100 p-6 rounded-3xl shadow-md flex flex-col items-center text-center pt-[60px]">
+          <h3 className="text-2xl font-bold mb-0 md:mb-4">Meet Ayu</h3>
+          <img
+            src="/ayu-image.svg"
+            alt="Ayu bot"
+            className="h-[328px] w-auto object-contain mb-0 md:mb-4"
+          />
+          <p className="text-lg text-left text-gray-600 max-w-md">
+            Ayu is more than just a reminder—it’s a personalized digital
+            companion that fosters emotional connection and sustained
+            engagement.
+          </p>
+        </div>
+      
         {/* Reminders block */}
-        <div className="flex items-center gap-4">
+        <div className="flex md:flex-col-reverse
+        justify-center
+         2xl:flex-row items-center gap-4">
           <div>
             <h3 className="font-bold text-2xl">Reminders</h3>
             <p className="text-lg text-gray-600">
@@ -48,20 +65,7 @@ const HeroSection = () => {
           <img src="/clock.svg" alt="clock" className="h-[110px] w-[110px]" />
         </div>
 
-        {/* Meet Ayu card */}
-        <div className="bg-gray-100 p-6 rounded-3xl shadow-md flex flex-col items-center text-center pt-[60px]">
-          <h3 className="text-2xl font-bold mb-4">Meet Ayu</h3>
-          <img
-            src="/ayu-image.svg"
-            alt="Ayu bot"
-            className="h-[328px] w-auto object-contain mb-4"
-          />
-          <p className="text-lg text-left text-gray-600 max-w-md">
-            Ayu is more than just a reminder—it’s a personalized digital
-            companion that fosters emotional connection and sustained
-            engagement.
-          </p>
-        </div>
+       
       </div>
     </div>
   );
